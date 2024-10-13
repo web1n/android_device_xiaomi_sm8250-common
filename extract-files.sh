64e8b5a6ed7a0605918a6a9aa9775a8a7ec70a87
Bin <9633157+web1n@users.noreply.github.com>
2024-10-13 10:44:07 +0800

    sm8250-common: Link libwvdrmengine against libcrypto from vndk 33
    libwvdrmengine.so: error: Unresolved symbol: CBS_init

--- extract-files.sh
+++ extract-files.sh
@@ -82,7 +82,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
             ;;
-        vendor/lib64/libwvhidl.so)
+        vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
