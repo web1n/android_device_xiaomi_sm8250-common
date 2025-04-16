d50d8e8892750e0aa0fcbc2852d282da7ba5fc71
Bin <9633157+web1n@users.noreply.github.com>
2025-04-16 17:15:08 +0800

    sm8250-common: Exclude NFC for tablet

--- kona.mk
+++ kona.mk
@@ -255,6 +255,7 @@ PRODUCT_PACKAGES += \
     video_system_specs.json
 
 # NFC
+ifneq ($(TARGET_IS_TABLET),true)
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
     com.android.nfc_extras \
@@ -269,6 +270,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml
+endif
 
 # OMX
 TARGET_SUPPORTS_OMX_SERVICE := false
